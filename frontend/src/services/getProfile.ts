35480bb1dcc329c8c7afa517c107eaa4efcc4f87
Terrence Daniels <terrence_daniels_35@yahoo.com>
2026-08-18 10:41:50 -0400

    Add frontend/src/services/getProfile.ts

---
+++ frontend/src/services/getProfile.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+import errorHandler from "../helpers/errorHandler";
+import type { Profile } from "../types";
+
+interface GetProfileParams {
+  headers?: Record<string, string>;
+  username: string;
+}
+
+interface ProfileResponse {
+  profile: Profile;
+}
+
+async function getProfile({
+  headers,
+  username,
+}: GetProfileParams): Promise<Profile | undefined> {
+  try {
+    const { data } = await axios<ProfileResponse>({
+      headers,
+      url: `/api/profiles/${username}`,
+    });
+
+    return data.profile;
+  } catch (error) {
+    if (axios.isAxiosError<{ errors: { body: string[] } }>(error)) {
+      errorHandler(error);
+    }
+  }
+}
+
+export default getProfile;
