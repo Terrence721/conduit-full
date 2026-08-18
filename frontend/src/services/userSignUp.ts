91b02acb62ac0a6befee22126e4c4383f3d71204
Terrence Daniels <terrence_daniels_35@yahoo.com>
2026-08-18 11:04:40 -0400

    Add frontend/src/services/userSignUp.ts

---
+++ frontend/src/services/userSignUp.ts
@@ -0,0 +1,42 @@
+import axios from "axios";
+import errorHandler from "../helpers/errorHandler";
+import type { AuthState, User } from "../types";
+
+interface UserSignUpParams {
+  username: string;
+  email: string;
+  password: string;
+}
+
+interface UserResponse {
+  user: User;
+}
+
+async function userSignUp({
+  username,
+  email,
+  password,
+}: UserSignUpParams): Promise<AuthState | undefined> {
+  try {
+    const { data } = await axios<UserResponse>({
+      data: { user: { username, email, password } },
+      method: "POST",
+      url: "/api/users",
+    });
+
+    const { user } = data;
+    const headers = { Authorization: `Token ${user.token}` };
+
+    const loggedIn: AuthState = { headers, isAuth: true, loggedUser: user };
+
+    localStorage.setItem("loggedUser", JSON.stringify(loggedIn));
+
+    return loggedIn;
+  } catch (error) {
+    if (axios.isAxiosError<{ errors: { body: string[] } }>(error)) {
+      errorHandler(error);
+    }
+  }
+}
+
+export default userSignUp;
