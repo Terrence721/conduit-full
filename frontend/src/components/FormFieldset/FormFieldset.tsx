721efec32582e1bed4390fb6815aec73c605210f
Terrence Daniels <terrence_daniels_35@yahoo.com>
2026-08-20 07:59:47 -0400

    Add frontend/src/components/FormFieldset/FormFieldset.tsx

---
+++ frontend/src/components/FormFieldset/FormFieldset.tsx
@@ -0,0 +1,46 @@
+import type { ChangeEventHandler, ReactNode } from "react";
+
+interface FormFieldsetProps {
+  autoFocus?: boolean;
+  children?: ReactNode;
+  handler: ChangeEventHandler<HTMLInputElement>;
+  minLength?: number;
+  name: string;
+  normal?: boolean;
+  placeholder?: string;
+  required?: boolean;
+  type: string;
+  value: string;
+}
+
+function FormFieldset({
+  autoFocus,
+  children,
+  handler,
+  minLength,
+  name,
+  normal,
+  placeholder,
+  required,
+  type,
+  value,
+}: FormFieldsetProps) {
+  return (
+    <fieldset className="form-group">
+      <input
+        autoFocus={autoFocus}
+        className={`form-control ${normal ? "" : "form-control-lg"}`}
+        minLength={minLength}
+        name={name}
+        onChange={handler}
+        placeholder={placeholder}
+        required={required}
+        type={type}
+        value={value}
+      />
+      {children}
+    </fieldset>
+  );
+}
+
+export default FormFieldset;
