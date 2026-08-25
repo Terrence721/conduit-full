175b93bafdb818d7370d6df0b95e3f6ba54afd81
Terrence Daniels <terrence_daniels_35@yahoo.com>
2026-08-25 11:46:14 -0400

    Add FavButton to the central components barrel

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,6 +1,7 @@
 export { default as ArticleTags } from "./ArticleTags/ArticleTags";
 export { default as BannerContainer } from "./BannerContainer/BannerContainer";
 export { default as ContainerRow } from "./ContainerRow/ContainerRow";
+export { default as FavButton } from "./FavButton/FavButton";
 export { default as FormFieldset } from "./FormFieldset/FormFieldset";
 export { default as NavItem } from "./NavItem/NavItem";
 export { default as SourceCodeLink } from "./SourceCodeLink/SourceCodeLink";
