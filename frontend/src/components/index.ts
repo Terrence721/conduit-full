73c7dc199a82908b46bc983da8aa3758e92ee96c
Terrence Daniels <terrence_daniels_35@yahoo.com>
2026-08-28 08:45:10 -0400

    Add ArticlesPagination to the central components barrel

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,3 +1,4 @@
+export { default as ArticlesPagination } from "./ArticlesPagination/ArticlesPagination";
 export { default as ArticleTags } from "./ArticleTags/ArticleTags";
 export { default as BannerContainer } from "./BannerContainer/BannerContainer";
 export { default as ContainerRow } from "./ContainerRow/ContainerRow";
