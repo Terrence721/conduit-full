257cb3f9cf7c37cadfe4b52ee45c50dc9d7b84e2
Terrence Daniels <terrence_daniels_35@yahoo.com>
2026-09-01 11:50:14 -0400

    Add frontend/src/routes/Profile/Profile.tsx
    Direct port, no changes needed. This is the real future consumer of
    NavItem's state?: unknown prop, added back when NavItem itself was
    built.

---
+++ frontend/src/routes/Profile/Profile.tsx
@@ -0,0 +1,36 @@
+import { Outlet, useLocation } from "react-router-dom";
+import AuthorInfo from "../../components/AuthorInfo/AuthorInfo";
+import ContainerRow from "../../components/ContainerRow/ContainerRow";
+import NavItem from "../../components/NavItem/NavItem";
+
+function Profile() {
+  const { state } = useLocation();
+
+  return (
+    <div className="profile-page">
+      <div className="user-info">
+        <ContainerRow>
+          <AuthorInfo />
+        </ContainerRow>
+      </div>
+
+      <ContainerRow>
+        <div className="col-xs-12 col-md-10 offset-md-1">
+          <div className="articles-toggle">
+            <ul className="nav nav-pills outline-active">
+              <NavItem text="My Articles" url="" state={state} />
+              <NavItem
+                text="Favorited Articles"
+                url="favorites"
+                state={state}
+              />
+            </ul>
+          </div>
+          <Outlet />
+        </div>
+      </ContainerRow>
+    </div>
+  );
+}
+
+export default Profile;
