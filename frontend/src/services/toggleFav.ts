29eec70af96e4c784b9ec039f6b3b39893cf767e
Terrence Daniels <terrence_daniels_35@yahoo.com>
2026-09-07 18:06:42 -0400

    Simplify toggleFav.ts's catch block (frontend DRY finding #3, part 12/16)

--- frontend/src/services/toggleFav.ts
+++ frontend/src/services/toggleFav.ts
@@ -22,9 +22,7 @@ async function toggleFav({
 
     return data.article;
   } catch (error) {
-    if (axios.isAxiosError<{ errors: { body: string[] } }>(error)) {
-      errorHandler(error);
-    }
+    errorHandler(error);
   }
 }
 
