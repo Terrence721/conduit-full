45a17f2b7eec1d9d2f6c3f9d89196ca91decd0db
Terrence Daniels <terrence_daniels_35@yahoo.com>
2026-09-07 17:08:49 -0400

    Wire ProfileArticles.tsx to ArticlesListView (frontend DRY finding #1, part 3/4)
    Same replacement as HomeArticles.tsx. Visually verified: a profile's
    "My Articles" tab renders identically, zero console errors.

--- frontend/src/routes/Profile/ProfileArticles.tsx
+++ frontend/src/routes/Profile/ProfileArticles.tsx
@@ -1,39 +1,16 @@
 import { useParams } from "react-router-dom";
-import ArticlesPagination from "../../components/ArticlesPagination/ArticlesPagination";
-import ArticlesPreview from "../../components/ArticlesPreview/ArticlesPreview";
-import useArticles from "../../hooks/useArticles";
+import ArticlesListView from "../../components/ArticlesListView/ArticlesListView";
 
 function ProfileArticles() {
   const { username } = useParams();
 
-  const { articles, articlesCount, loading, setArticlesData } = useArticles({
-    location: "profile",
-    username,
-  });
-
-  return loading ? (
-    <div className="article-preview">
-      <em>Loading {username} articles...</em>
-    </div>
-  ) : articles.length > 0 ? (
-    <>
-      <ArticlesPreview
-        articles={articles}
-        loading={loading}
-        updateArticles={setArticlesData}
-      />
-
-      <ArticlesPagination
-        articlesCount={articlesCount}
-        location="profile"
-        updateArticles={setArticlesData}
-        username={username}
-      />
-    </>
-  ) : (
-    <div className="article-preview">
-      {username} doesn&apos;t have articles.
-    </div>
+  return (
+    <ArticlesListView
+      emptyText={`${username} doesn't have articles.`}
+      loadingText={`Loading ${username} articles...`}
+      location="profile"
+      username={username}
+    />
   );
 }
 
